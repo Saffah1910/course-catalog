b17648548791bac1c2b1bb89ae0286dfcba8eb57
Saffah Khatib <skhatib1910@gmail.com>
2023-04-20 16:17:32 +0200

    intial commit

--- tutor-wages/tutorWageCalculator.js
+++ tutor-wages/tutorWageCalculator.js
@@ -2,7 +2,6 @@
 //Create a function called **tutorWageCalculator** that calculates how much a course tutor will earn based on this hourly rate
 // and hours worked. Each tutor got a level. 
 //the function should have 2 parameters one for the hours and one for the level
-
 //It takes a `timesheet string` & the tutor `level` of the tutor as parameters.
 //create a var for each level
 //inside the for loop the create and if staemnt with the conditions that if the level === 1 retrun the amount 
@@ -12,24 +11,30 @@
 
 function tutorWageCalculator(timesheetString, level){
  
-    var stringMap = {}
+    // var stringMap = {}
 
     var levelOne = 0;
     var levelTwo = 0;
     var levelThree = 0;
 
+
+
     for (var i = 0; i < level.length;i++){
 
         if(level[i] === 1){
-            levelOne += timesheetString
+            levelOne += 75 
         }
       
+        if(level[i] === 2){
+            levelTwo += 90
+        }
 
-        else if(level[i] === 2){
-            levelTwo += timesheetString
+        if(level[i] === 3){
+            levelThree += 105
         }
     }
     return levelOne
+
 
 
 
